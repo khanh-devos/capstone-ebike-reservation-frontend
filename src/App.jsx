0f504eb141af2b0a6ba0313d39c625889688ca04
Tchasinga jacques <tchasingajacques@gmail.com>
2023-11-02 15:50:50 +0200

    first commit
    In the setup side of this project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-xs text-red-600"> Hello World </h1>
+    </div>
+  );
+}
+
+export default App;
