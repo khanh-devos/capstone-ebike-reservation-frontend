07da991b93f08f8aa3cc5112bca4cae69f5f8d9c
Tchasinga jacques <tchasingajacques@gmail.com>
2023-11-02 15:55:35 +0200

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './index.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-xs text-red-600"> Hello World </h1>
+      <h1 className="flex justify-center mt-36 text-4xl"> Hello everyone and welcom to this final capstone project </h1>
     </div>
   );
 }
