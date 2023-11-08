8a0e2f150fd4937a70837b60ba02e3deef42f893
rod <rodrip0129@gmail.com>
2023-11-07 22:36:41 -0300

    add functionality to carousel

--- src/Components/ebikes/BikeCarousel.jsx
+++ src/Components/ebikes/BikeCarousel.jsx
@@ -7,37 +7,44 @@ const mockBikes = [
   {
     id: 1,
     name: 'EcoRider 500',
-    description: 'An elegant and efficient electric bicycle for your daily commute.',
+    description:
+      'An elegant and efficient electric bicycle for your daily commute.',
     image: 'https://media.bcompras.com.mx/iFSz4S0PZCusaaKK1RvtSbd2.webp',
   },
   {
     id: 2,
     name: 'VoltXpress ZR2',
-    description: 'The perfect companion to explore the city with speed and comfort.',
-    image: 'https://http2.mlstatic.com/D_NQ_NP_2X_904305-MLA48736887155_012022-F.webp',
+    description:
+      'The perfect companion to explore the city with speed and comfort.',
+    image:
+      'https://http2.mlstatic.com/D_NQ_NP_2X_904305-MLA48736887155_012022-F.webp',
   },
   {
     id: 3,
     name: 'UrbanGlide Pro',
-    description: 'A versatile and modern electric bicycle that adapts to your lifestyle.',
+    description:
+      'A versatile and modern electric bicycle that adapts to your lifestyle.',
     image: 'https://media.bcompras.com.mx/tVmMvJPOxrykcPSk2roMyqOx.webp',
   },
   {
     id: 4,
     name: 'EcoMotion X3',
-    description: 'Experimenta la libertad de moverte de manera sostenible con esta e-bike.',
+    description:
+      'Experimenta la libertad de moverte de manera sostenible con esta e-bike.',
     image: 'https://media.bcompras.com.mx/RUFTQuCiAjO0SVYA4SF4W3Po.webp',
   },
   {
     id: 5,
     name: 'SpeedRider S1',
-    description: 'Experience the freedom of moving sustainably with this e-bike.',
+    description:
+      'Experience the freedom of moving sustainably with this e-bike.',
     image: 'https://media.bcompras.com.mx/F0OGrsgu8eZV3kw2MLbpJnSJ.webp',
   },
   {
     id: 6,
     name: 'CityCommuter E7',
-    description: 'Ride around town in style and efficiency on this modern e-bike.',
+    description:
+      'Ride around town in style and efficiency on this modern e-bike.',
     image: 'https://media.bcompras.com.mx/yYuGNeaCO1J1aHHsQhbhzwbJ.webp',
   },
 ];
@@ -63,7 +70,7 @@ const BikeCarousel = () => {
   const moveSlide = (direction) => {
     if (direction === 'prev') {
       setCurrentIndex((currentIndex - 1 + mockBikes.length) % mockBikes.length);
-    } else {
+    } else if (direction === 'next') {
       setCurrentIndex((currentIndex + 1) % mockBikes.length);
     }
   };
@@ -72,7 +79,7 @@ const BikeCarousel = () => {
     <div className="carousel-container">
       <button
         type="button"
-        className="arrow-left"
+        className={`arrow-left ${currentIndex === 0 ? 'inactive' : 'active'}`}
         onClick={() => moveSlide('prev')}
       >
         {' '}
@@ -81,14 +88,18 @@ const BikeCarousel = () => {
       </button>
 
       <div className="bike-slides">
-        {mockBikes.slice(currentIndex, currentIndex + (isMobile ? 1 : 3)).map((bike) => (
-          <BikeCard key={bike.id} bike={bike} />
-        ))}
+        {mockBikes
+          .slice(currentIndex, currentIndex + (isMobile ? 1 : 3))
+          .map((bike) => (
+            <BikeCard key={bike.id} bike={bike} />
+          ))}
       </div>
 
       <button
         type="button"
-        className="arrow-right"
+        className={`arrow-right ${
+          currentIndex === mockBikes.length - 1 ? 'inactive' : 'active'
+        }`}
         onClick={() => moveSlide('next')}
       >
         {' '}
@@ -99,7 +110,6 @@ const BikeCarousel = () => {
   );
 };
 
-BikeCarousel.propTypes = {
-};
+BikeCarousel.propTypes = {};
 
 export default BikeCarousel;
