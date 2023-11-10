0a602c3206b7516c67a4d49d78ef2525c1615701
Tchasinga jacques <tchasingajacques@gmail.com>
2023-11-10 09:13:14 +0200

    new adding side

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,19 +1,17 @@
-// store.js
 import { configureStore } from '@reduxjs/toolkit';
-import thunk from 'redux-thunk'; // For handling async actions
 import ebikeSlice from './ebike/ebikeSlice';
 import authSlice from './auth/authSlice';
+
 import locationSlice from './location/locationSlice';
-import myReservationSlice from './reservation/myReservationSlice';
+import reservationSlice from './reservation/reservationSlice';
 
 const store = configureStore({
   reducer: {
     locationSlice,
     authSlice,
-    myReservationSlice,
     ebikeSlice,
+    reservationSlice,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(thunk),
 });
 
 export default store;
