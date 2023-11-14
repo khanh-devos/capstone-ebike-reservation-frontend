665a4a88ac82a8fc3c1c0a7919ee8fd9331c5ce3
souad988 <souadelmansouri88@gmail.com>
2023-11-14 16:36:54 +0100

    Add screensize state

--- src/Components/reservations/MyReservations.jsx
+++ src/Components/reservations/MyReservations.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchReservations } from '../../redux/reservation/reservationSlice';
 import './reservation.css';
@@ -7,6 +7,24 @@ const MyReservations = () => {
   const dispatch = useDispatch();
   const { reservations } = useSelector((state) => state.reservationSlice);
   const { user } = useSelector((state) => state.authSlice);
+
+  const [screenSize, setScreenSize] = useState('');
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.matchMedia('(max-width: 768px)').matches) {
+        setScreenSize('small');
+      } else {
+        setScreenSize('large');
+      }
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Initialize with current screen size
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
   useEffect(() => {
     dispatch(fetchReservations());
   }, [dispatch]);
@@ -15,18 +33,20 @@ const MyReservations = () => {
       <h1 className="title">
         {`${user.name.toUpperCase()}'s Reservations`}
       </h1>
-      <div className="container">
-        { reservations && reservations.length > 0
-          ? (
-            <div className="table">
-              <div className="table_row">
-                <div className="table_header_item">Ebike</div>
-                <div className="table_header_item">Location</div>
-                <div className="table_header_item">From</div>
-                <div className="table_header_item">To</div>
+      
+        : (
+          <div className="container">
+            { reservations && reservations.length > 0
+              ? (
+                <div className="table">
+                  <div className="table_row">
+                    <div className="table_header_item">Ebike</div>
+                    <div className="table_header_item">Location</div>
+                    <div className="table_header_item">From</div>
+                    <div className="table_header_item">To</div>
 
-              </div>
-              {
+                  </div>
+                  {
             reservations
               .filter((item) => item.user_id === user.id)
               .map((reservation) => (
@@ -39,10 +59,11 @@ const MyReservations = () => {
                 </div>
               ))
             }
-            </div>
-          )
-          : <p>no reservations</p>}
-      </div>
+                </div>
+              )
+              : <p>no reservations</p>}
+          </div>
+        )}
     </div>
   );
 };
