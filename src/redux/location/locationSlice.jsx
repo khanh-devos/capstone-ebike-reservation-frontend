5db6606cbe418e0914a21101a993f45681552f1a
souad988 <souadelmansouri88@gmail.com>
2023-11-14 15:41:05 +0100

    Add env url to locations slice

--- src/redux/location/locationSlice.jsx
+++ src/redux/location/locationSlice.jsx
@@ -1,6 +1,7 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import axios from 'axios';
 
+const backendURL = process.env.REACT_APP_BACKEND_URL;
 const LOCATION_URL = 'http://localhost:3100/api/v1/locations';
 
 const initialState = {
